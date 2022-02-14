c1123f0ca7c90520b46613811c21b0ace3156077
GameRickster <66086926+GameRickster2020@users.noreply.github.com>
2022-02-14 11:39:36 -0800

    Update rotation.cs

--- rotation.cs
+++ rotation.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class rotation : MonoBehaviour
 {
-    private const float V = 2.0f;
-    public float delta = 1.5f;  // Amount to move left and right from the start point
-    public float rotate = V;
-    private Transform startPos;
-
-
-    void Update()
+     void Update()
     {
         transform.Rotate(0, 1, 0, Space.World);
 
