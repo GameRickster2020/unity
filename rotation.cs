bbb596e52736a856f055838cd9c82b2b7fa1c34f
GameRickster <66086926+GameRickster2020@users.noreply.github.com>
2022-02-14 11:38:58 -0800

    Add files via upload
    Simple object rotation code for Unity.

---
+++ rotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    private const float V = 2.0f;
+    public float delta = 1.5f;  // Amount to move left and right from the start point
+    public float rotate = V;
+    private Transform startPos;
+
+
+    void Update()
+    {
+        transform.Rotate(0, 1, 0, Space.World);
+
+
+    }
+}
